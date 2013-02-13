c46474750bd361048149ee5dcfa290ab4d9648e0
Travis Horn <horn.travis@gmail.com>
2013-02-13 12:45:48 -0500

    Refactored code.

--- js/imgClicked.js
+++ js/imgClicked.js
@@ -1,23 +1,28 @@
 function imgClicked(pageX, pageY) {
-	var highestZIndex = -1;
-	var highestElement = null;
-	
-	var clickedImages = imagesAt(pageX, pageY);
+	var highestZIndex  = -1,
+        highestElement = null,
+        clickedImages  = imagesAt(pageX, pageY);
 	
 	clickedImages.each(function(){
-		var self = $(this);
-		var img = new Image();
-		img.src = self.attr('src');
-		var canvas = document.createElement('canvas');
-		canvas.width = img.width;
+		var self   = $(this),
+            img    = new Image(),
+            canvas = document.createElement('canvas'),
+            ctx    = canvas.getContext('2d'),
+            imgData,
+            imgClickX,
+            imgClickY;
+        
+        img.src       = self.attr('src');
+		canvas.width  = img.width;
 		canvas.height = img.height;
-		var ctx = canvas.getContext('2d');
+        
 		ctx.drawImage(img, 0, 0);
-		var imgData = ctx.getImageData(0, 0, img.width, img.height);
+        
+		imgData   = ctx.getImageData(0, 0, img.width, img.height);
 		imgClickX = pageX - self.offset().left;
 		imgClickY = pageY - self.offset().top;
 		
-		if (imgData.data[(imgClickX*img.width+imgClickY)*4+3] != 0) {
+		if (imgData.data[(imgClickX*img.width+imgClickY)*4+3] !== 0) {
 			if (self.css('z-index') > highestZIndex) {
 				highestElement = self;
 			}
@@ -29,15 +34,14 @@ function imgClicked(pageX, pageY) {
 
 function imagesAt(x, y){
 	var $images = $('img').map(function(){
-		var $this = $(this);
-		var offset = $this.offset();
-		var l = offset.left;
-		var t = offset.top;
-		var h = $this.height();
-		var w = $this.width();
-
-		var maxx = l + w;
-		var maxy = t + h;
+		var $this  = $(this),
+            offset = $this.offset(),
+            l      = offset.left,
+            t      = offset.top,
+            h      = $this.height(),
+            w      = $this.width(),
+            maxx   = l + w,
+            maxy   = t + h;
 
 		return (y <= maxy && y >= t) && (x <= maxx && x >= l) ? $this : null;
 	});
